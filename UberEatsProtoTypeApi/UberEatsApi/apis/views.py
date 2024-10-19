7ddd5fa4f65ce13daf6ad65c718f17348e8e1492
Yashasvi <yashasvikanchugantla@gmail.com>
2024-10-19 16:31:54 -0700

    update profile picture api is added

--- UberEatsProtoTypeApi/UberEatsApi/apis/views.py
+++ UberEatsProtoTypeApi/UberEatsApi/apis/views.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
+
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
@@ -22,6 +23,32 @@ class CustomerViewSet(viewsets.ModelViewSet):
                 return Response(serializer.data)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @action(detail=False, methods=['GET', 'PUT', 'PATCH'])
+    def profile(self, request):
+        try:
+            customer = Customer.objects.get(user=request.user)
+        except Customer.DoesNotExist:
+            return Response({'error': 'Customer profile not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        if request.method == 'GET':
+            serializer = self.get_serializer(customer)
+            return Response(serializer.data)
+
+        elif request.method in ['PUT', 'PATCH']:
+            serializer = self.get_serializer(customer, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=['PUT'], url_path='profile-picture')
+    def update_profile_picture(self, request):
+        customer = Customer.objects.get(user=request.user)
+        if 'profile_image' in request.data:
+            customer.profile_image = request.data['profile_image']
+            customer.save()
+            return Response({'message': 'Profile picture updated successfully'}, status=status.HTTP_200_OK)
+        return Response({'error': 'No image file provided'}, status=status.HTTP_400_BAD_REQUEST)
 class RestaurantViewSet(viewsets.ModelViewSet):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
