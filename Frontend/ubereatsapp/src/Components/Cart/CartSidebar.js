53c2d07f4ea403f2f16198611feb99162279a66d
naikn <kanchannaik55@gmail.com>
2024-10-28 13:33:18 -0700

    changes for checkout add

--- Frontend/ubereatsapp/src/Components/Cart/CartSidebar.js
+++ Frontend/ubereatsapp/src/Components/Cart/CartSidebar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './CartSidebar.css'; // Change to the new CSS file for the sidebar
+import './CartSidebar.css';
 import Cookies from 'js-cookie';
 import { useDispatch } from 'react-redux';
 import { setCartItems } from '../../Reducers/cartReducer';
@@ -10,29 +10,29 @@ import { messageService } from '../Common/Message/MessageService';
 
 const CartSidebar = ({ closeCart }) => {
   const [cartItems, setCartItemsState] = useState([]); 
-  const [restaurantName, setRestaurantName] = useState('')// Local state for cart items
+  const [restaurantName, setRestaurantName] = useState('')
   const navigate = useNavigate()
   const dispatch = useDispatch();
 
-  // Function to fetch cart data
+ 
   const fetchCartData = async () => {
     try {
       const response = await axios.get(`${BASE_API_URL}/api/cart/get_cart`, {
-        withCredentials: true, // Enable sending cookies with the request
+        withCredentials: true,
         headers: {
           'Content-Type': 'application/json',
           'X-CSRFToken': Cookies.get('csrftoken'),
         },
       });
-      const items = response.data.items; // Assuming response.data.items contains the cart items
+      const items = response.data.items; 
 
-      // Map items to include only dish_id and quantity
+      
       const formattedItems = items.map((item) => ({
-        dish_id: item.dish_id, // Assuming item.id is the dish ID
-        quantity: item.quantity || 1, // Get the quantity or default to 1
+        dish_id: item.dish_id, 
+        quantity: item.quantity || 1,
       }));
 
-      setCartItemsState(items); // Update local state
+      setCartItemsState(items); 
       dispatch(setCartItems({ items: formattedItems, restaurantId: response.data.restaurant_id, reset: true })); // Dispatch action to update Redux store with formatted items
     } catch (error) {
       messageService.showMessage('error', 'Error ins fetching cart');
@@ -41,14 +41,12 @@ const CartSidebar = ({ closeCart }) => {
   };
 
   useEffect(() => {
-    fetchCartData(); // Fetch cart data when component mounts
+    fetchCartData(); 
   }, []);
 
   const handleQuantityChange = (id, event) => {
-    const value = parseInt(event.target.value); // Get the value from the event directly
-    // Ensure the quantity is a valid number
+    const value = parseInt(event.target.value); 
     if (value >= 1) {
-      // Update cartItems directly
       setCartItemsState((prevItems) =>
         prevItems.map((item) =>
           item.dish_id === id ? { ...item, quantity: value } : item
@@ -57,18 +55,17 @@ const CartSidebar = ({ closeCart }) => {
     }
   };
 
-  // Calculate subtotal
   const calculateSubtotal = () => {
     return cartItems.reduce((total, item) => {
-      return total + (item.price * (item.quantity || 1)); // Calculate total for each item based on its quantity
+      return total + (item.price * (item.quantity || 1));
     }, 0).toFixed(2);
   };
 
-  // Function to get the formatted items
+ 
   const getFormattedItems = () => {
     return cartItems.map((item) => ({
-      dish_id: item.dish_id, // Assuming item.id is the dish ID
-      quantity: item.quantity || 1, // Get the quantity or default to 1
+      dish_id: item.dish_id, 
+      quantity: item.quantity || 1, 
     }));
   };
 
@@ -76,8 +73,8 @@ const CartSidebar = ({ closeCart }) => {
   const handleCheckout = async () => {
     try {
       const formattedItems = cartItems.map((item) => ({
-        dish_id: item.dish_id, // Assuming item.id is the dish ID
-        quantity: item.quantity || 1, // Get the quantity or default to 1
+        dish_id: item.dish_id, 
+        quantity: item.quantity || 1, 
       }));
 
       const response = await axios.post(
@@ -90,12 +87,12 @@ const CartSidebar = ({ closeCart }) => {
                   'Content-Type': 'application/json',
                   'X-CSRFToken': Cookies.get('csrftoken'),
               },
-              withCredentials: true, // Ensure that cookies are included in the request
+              withCredentials: true, 
           }
       );
       
       console.log('Response:', response.data);
-      closeCart() // Handle the response data here
+      closeCart()
       messageService.showMessage('success', 'Item succesfully added in cart')
       navigate('/order/checkout')
   } catch (error) {
@@ -107,18 +104,18 @@ const CartSidebar = ({ closeCart }) => {
   const handleClearCart = async () => {
     try {
       await axios.delete(`${BASE_API_URL}/api/cart/clear_cart/`, {
-        withCredentials: true, // Enable sending cookies with the request
+        withCredentials: true, 
         headers: {
           'Content-Type': 'application/json',
           'X-CSRFToken': Cookies.get('csrftoken'),
         },
       });
 
-      // Clear local cartItems state
+     
       setCartItemsState([]);
       dispatch(setCartItems({ items: [], reset: true }));
       messageService.showMessage('success', 'Cleared cart succefully')
-      closeCart() // Dispatch to Redux to clear the cart
+      closeCart() 
     } catch (error) {
       console.error('Error clearing the cart:', error);
       messageService.showMessage('success', 'Error in clearing cart')
@@ -137,16 +134,16 @@ const CartSidebar = ({ closeCart }) => {
         <hr />
 
         {cartItems.map((item) => (
-          <div key={item.dish_id} className="item-details"> {/* Use item.id for key */}
-            <h3>{item.dish_name}</h3> {/* Assuming item has a dish_name property */}
-            <p>{item.description}</p> {/* Assuming item has a description property */}
+          <div key={item.dish_id} className="item-details"> 
+            <h3>{item.dish_name}</h3> 
+            <p>{item.description}</p> 
 
             <div className="quantity-section">
               <label htmlFor={`quantity-${item.dish_id}`}>Qty:</label>
               <select
                 id={`quantity-${item.dish_id}`}
-                value={item.quantity || 1} // Use individual quantity from item
-                onChange={(e) => handleQuantityChange(item.dish_id, e)} // Pass the entire event
+                value={item.quantity || 1} 
+                onChange={(e) => handleQuantityChange(item.dish_id, e)} 
               >
                 {[...Array(10).keys()].map((num) => (
                   <option key={num + 1} value={num + 1}>
@@ -154,7 +151,7 @@ const CartSidebar = ({ closeCart }) => {
                   </option>
                 ))}
               </select>
-              <p>${(item.price * (item.quantity || 1)).toFixed(2)}</p> {/* Calculate price per item */}
+              <p>${(item.price * (item.quantity || 1)).toFixed(2)}</p> 
             </div>
           </div>
         ))}
@@ -163,12 +160,12 @@ const CartSidebar = ({ closeCart }) => {
 
         <div className="subtotal-section">
           <h6>Subtotal</h6>
-          <p>${calculateSubtotal()}</p> {/* Display calculated subtotal */}
+          <p>${calculateSubtotal()}</p> 
         </div>
 
         <div className="button-container">
         <button className="checkout-btn" onClick={handleCheckout}>Go to checkout</button>
-        <button className="checkout-btn" onClick={handleClearCart}>Clear Cart</button>  {/* Button to clear cart */}
+        <button className="checkout-btn" onClick={handleClearCart}>Clear Cart</button>  
         </div>
       </div>
     </div>
